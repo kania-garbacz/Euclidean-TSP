f032d5a5644d29f74832110119727cb34a430cd7
Dominik Kania <doka@netcompany.com>
2023-01-23 21:37:26 +0100

    feat: added sending data to csv file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,69 +9,68 @@ import java.util.List;
 
 public class Main {
     static List<Point> points = new ArrayList<>();
-
-    private Runtime rt = Runtime.getRuntime();
-    private long total_mem = rt.totalMemory();
-    private long free_mem = rt.freeMemory();
+    private static List<String[]> statisticlist;
 
 
     public static void main(String[] args) {
+        String[] header = {"Liczba punktow", "Czas dzialania (ms)", "Liczba obiektow"};
+
+        statisticlist = new ArrayList<>();
+        statisticlist.add(header);
+
         testBruteForce();
 //        testNearestNeighbour();
 
-
-        List<String[]> csvData = createCsvDataSpecial();
-
         // default all fields are enclosed in double quotes
         // default separator is a comma
-        try (CSVWriter writer = new CSVWriter(new FileWriter("./result.csv"))) {
-            writer.writeAll(csvData);
+        try (CSVWriter writer = new CSVWriter(new FileWriter("./result.csv"),';', '"', '"', "\n")) {
+            writer.writeAll(statisticlist);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static List<String[]> createCsvDataSpecial() {
-
-        String[] header = {"Liczba punktow", "Czas dzialania (ms)", "Liczba obiektow"};
-        String[] record1 = {"Dell", "P3421W", "Dell 34, Curved, USB-C Monitor", "2499.00"};
-        // embedded double quotes
-
-        List<String[]> list = new ArrayList<>();
-        list.add(header);
-        list.add(record1);
-
-
-        return list;
-
-    }
-
     private static void testBruteForce() {
         System.out.println("Brute force test");
 
-        for (int counter = 5; counter < 11; counter++) {
-            points = Utils.generatePoints(counter, -3, 10);
+        int repeatCount = 10;
+        long timeCount;
+        long memoryCount;
+
+        for (int counter = 1; counter < 11; counter++) {
+            timeCount = 0;
+            memoryCount = 0;
+
+            for (int repeats = 0; repeats < repeatCount; repeats++) {
+                points = Utils.generatePoints(counter, -3, 10);
 
 
-            BruteForce bruteForce = new BruteForce(points);
+                BruteForce bruteForce = new BruteForce(points);
 
-            List<Integer> pointsNums = new ArrayList<>();
-            for (int i = 0; i < points.size(); i++) {
-                pointsNums.add(i);
+                List<Integer> pointsNums = new ArrayList<>();
+                for (int i = 0; i < points.size(); i++) {
+                    pointsNums.add(i);
+                }
+
+                long start = System.currentTimeMillis();
+//        Metoda generująca najkrótszą ścieżkę Hamiltona
+                bruteForce.permute(new Route(), pointsNums);
+                long end = System.currentTimeMillis();
+
+                timeCount += (end - start);
+                memoryCount += bruteForce.getMemorySize();
+
+                System.out.println("Liczba puntkow: " + counter);
+                System.out.println("Najkrotsza trasa: ");
+                System.out.println(bruteForce.getNajkrotszaTrasa().toString());
+                System.out.println("Dlugosc najkrotszej trasy: " + bruteForce.getNajkrotszyDystans());
+                System.out.println("Czas dzialania algorytmu: " + (end - start) + " ms");
+                System.out.println("Zuzycie pamieci: " + bruteForce.getMemorySize());
             }
 
-            long start = System.currentTimeMillis();
-//        Metoda generująca najkrótszą ścieżkę Hamiltona
-            bruteForce.permute(new Route(), pointsNums);
-            long end = System.currentTimeMillis();
 
-
-            System.out.println("Liczba puntkow: " + counter);
-            System.out.println("Najkrotsza trasa: ");
-            System.out.println(bruteForce.getNajkrotszaTrasa().toString());
-            System.out.println("Dlugosc najkrotszej trasy: " + bruteForce.getNajkrotszyDystans());
-            System.out.println("Czas dzialania algorytmu: " + (end - start) + " ms");
-            System.out.println("Zuzycie pamieci: " + bruteForce.getMemorySize());
+            String[] csvData = {String.valueOf(counter), String.valueOf((double) timeCount / repeatCount), String.valueOf(memoryCount / repeatCount)};
+            statisticlist.add(csvData);
         }
     }
 
